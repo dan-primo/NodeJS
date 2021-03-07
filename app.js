88e0f49f651c4db169283c4d29edf8e902358650
dan-primo <danielly.primo@gmail.com>
2021-03-07 13:16:05 -0300

    Step3-Improving-Project-Handling-Errors

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const rotaProdutos = require('.routes/produtos');
-const rotaPedidos = require('.routes/pedidos');
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use(morgan('dev'));
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+// Tratativa de erros para rotas não encontradas
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        error: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
